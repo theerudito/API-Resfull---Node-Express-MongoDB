de8bc63f56ea0ba13cb4052a5e98305dc1452f3c
theerudito <erudito.tv@gmail.com>
2022-01-30 14:41:15 -0500

    v1

--- index.js
+++ index.js
@@ -15,9 +15,35 @@ app.get("/", (req, res) => {
   res.send("Holas")
 })
 
+const api = "https://api-resfull-byerudito.herokuapp.com/api/productos" 
 
 
-const port = process.env.PORT || 2000
+      
+  const obtenerDatos =  async (apiURL) => {
+        try {
+      const res = await fetch(apiURL);
+      console.log(res);
+      const data = await res.json();
+      console.log(data);
+             /* {
+                /*imprimirDatos(data)
+                siguiente(data.info)
+                anterior(data.info)
+              }*/
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+   }
+
+   obtenerDatos(api)   
+
+
+
+  
+
+
+
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log("SERVIDOR A SU SERVICIO EN EL PUERTO", port))
 
 
